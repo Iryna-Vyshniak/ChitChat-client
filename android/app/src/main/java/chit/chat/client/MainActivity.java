caa676eac3c6bb34877f604d732efb257c124ba9
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-08 00:23:28 +0300

    init

---
+++ android/app/src/main/java/chit/chat/client/MainActivity.java
@@ -0,0 +1,5 @@
+package chit.chat.client;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'chit.chat.client',
+  appName: 'ChitChat-client',
+  webDir: 'build'
+};
+
+export default config;
