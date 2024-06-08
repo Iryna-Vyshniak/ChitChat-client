0e49e6ed96fecf4e95e2d5981d5212da5a46d801
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-08 22:40:51 +0300

    chore: Updated Capacitor webDir from 'build' to 'dist'
    - Modified `CapacitorConfig` to change the `webDir` property:
      - Previous value: 'build'
      - Updated value: 'dist'
    
    This change ensures that the Capacitor configuration correctly points to the `dist` directory for the web assets.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'chit.chat.client',
   appName: 'ChitChat-client',
-  webDir: 'build'
+  webDir: 'dist'
 };
 
 export default config;
