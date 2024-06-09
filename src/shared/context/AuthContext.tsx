d529b6f826bb99f88617e8c5e730e093542527ba
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-09 23:43:27 +0300

    feat: Implemented AuthContext for user authentication
    - Create AuthContext using React's createContext API.
    - Define useAuthContext hook for accessing the AuthContext.
    - Define AuthContextProvider component:
      - It maintains authUser state representing the authenticated user.
      - It loads the authenticated user from preferences on component mount.
      - It provides authUser and its setter function to the context value.

---
+++ src/shared/context/AuthContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { Preferences } from '@capacitor/preferences';
+
+import { AuthContextType, UserI } from '../types';
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const useAuthContext = (): AuthContextType => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuthContext must be used within an AuthContextProvider');
+    }
+    return context;
+};
+
+export const AuthContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [authUser, setAuthUser] = useState<UserI | null>(null);
+
+  useEffect(() => {
+    const loadUserFromPreferences = async () => {
+      try {
+        const storedUser = await Preferences.get({ key: 'user' });
+        if (storedUser?.value) {
+          setAuthUser(JSON.parse(storedUser.value));
+        }
+      } catch (error) {
+        console.error('Error loading user from preferences:', error);
+      }
+    };
+
+    loadUserFromPreferences();
+
+    // Clean up
+    return () => setAuthUser(null);
+  }, []);
+
+  return <AuthContext.Provider value={{ authUser, setAuthUser }}>{children}</AuthContext.Provider>;
+};
