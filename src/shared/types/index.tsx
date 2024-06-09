8c28bc17b7c5647ed1e0b2304ba20768bd0581cf
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-09 23:45:40 +0300

    feat: Defined interfaces for user authentication and form validation
    - Define UserI interface for user data.
    - Define AuthContextType interface for authentication context.
    - Define SignupI and SigninI interfaces for signup and signin data.
    - Define ValidationErrors type for form validation errors.
    - Define UseFormProps interface for form properties.
    - Define SignUpFormProps interface for signup form properties.
    - Define SelectedGenderProps interface for selected gender properties.

--- src/shared/types/index.tsx
+++ src/shared/types/index.tsx
@@ -1,10 +1,56 @@
 export interface AppPage {
-    url: string;
-    iosIcon: string;
-    mdIcon: string;
-    title: string;
-  }
+  url: string;
+  iosIcon: string;
+  mdIcon: string;
+  title: string;
+}
 
-  export interface  MenuProps {
-    isMobile: boolean;
-  }
+export interface MenuProps {
+  isMobile: boolean;
+}
+
+// AUTH
+export interface UserI {
+  _id: string;
+  fullName: string;
+  username: string;
+  avatar: string;
+  email: string;
+}
+
+export interface AuthContextType {
+  authUser: UserI | null;
+  setAuthUser: (user: UserI | null) => void;
+}
+
+export interface SignupI {
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  gender: string;
+}
+
+export interface SigninI {
+  email: string;
+  password: string;
+}
+
+export type ValidationErrors<T> = Partial<Record<keyof T, string>>;
+
+export interface UseFormProps<T> {
+  initialValues: T;
+  validate: (values: T) => ValidationErrors<T>;
+  onSubmit: (values: T) => void;
+}
+
+export interface SignUpFormProps {
+  onSubmit: (values: SignupI) => Promise<void>;
+}
+
+export interface SelectedGenderProps {
+  selectedGender: string;
+  handleChange: (e: CustomEvent<any>) => Promise<void>;
+  errors: ValidationErrors<SignupI>;
+}
