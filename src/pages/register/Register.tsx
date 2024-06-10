288107a0993abbaff61813f1c324e7d0e3787098
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-10 03:51:30 +0300

    feat: Completed Register component with signup form

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,18 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {  IonContent,  IonHeader,  IonPage,  IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 
+import SignUpForm from '../../components/forms/SignUpForm';
+
+import { useSignUp } from '../../shared/hooks/auth/useSignUp';
+import { SignupI } from '../../shared/types';
+
 const Register: React.FC = () => {
+    const { signup } = useSignUp();
+
+    const onSubmit = async (values: SignupI): Promise<void> => {
+        await signup(values);
+    }
 
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Signup</IonTitle>
+            <IonHeader className="ion-no-border">
+                <IonToolbar color={'primary'}>
+                    <IonTitle>Sign Up</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent className="ion-padding">
-                UI goes here...
+            <IonContent scrollY={false} className="ion-padding">
+              <SignUpForm onSubmit={onSubmit} />
             </IonContent>
+          
         </IonPage>
     );
 };
