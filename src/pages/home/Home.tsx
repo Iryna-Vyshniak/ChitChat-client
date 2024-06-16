ee5157441dcb4fb92be39abcfa2fe3b53e6c046c
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-17 00:07:17 +0300

    feat: Dynamically render components based on route parameter

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,35 +1,44 @@
 import {
   IonButtons,
+  IonCol,
   IonContent,
   IonHeader,
   IonMenuButton,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
 import './Home.css';
 import { useParams } from 'react-router';
-import ExploreContainer from '../../components/ExploreContainer';
+import Chats from '../chats/Chats';
+
+const componentsMap: { [key: string]: React.FC<{ name: string }> } = {
+  Chats: Chats,
+};
 
 const Home: React.FC = () => {
-  const { name } = useParams<{ name: string;}>();
+  const { name } = useParams<{ name: string }>();
+
+  const SelectedComponent = componentsMap[name] || (() => <div>Page Not Found</div>);
+
   return (
     <>
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{name}</IonTitle>
+              <IonButtons slot='start'>
+                <IonMenuButton />
+              </IonButtons>
+              <IonTitle>{name}</IonTitle>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader collapse='condense'>
           <IonToolbar>
-            <IonTitle size="large">{name}</IonTitle>
+            <IonTitle size='large'>{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />
+        <SelectedComponent name={name} />
       </IonContent>
     </>
   );
