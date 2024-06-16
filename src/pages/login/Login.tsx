bee7a725d7b127534c156ecbddd0c5fc02c89adb
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-16 15:30:38 +0300

    feat: Completed functionality and UI Login component

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,32 +1,54 @@
-import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import React from 'react';
 
+import '../../theme/authstyles.css';
+
+import { useSignIn } from '../../shared/hooks/auth/useSignIn';
+
+import SignInForm from '../../components/forms/SignInForm';
+import { SigninI } from '../../shared/types';
+import Action from '../../components/forms/Action';
+
 interface LoginProps {
-    login: () => void;
-  }
+  login: () => void;
+}
 
-const Login: React.FC<LoginProps> = ({ login }) => {
-    const router = useIonRouter();
+const Login: React.FC<LoginProps> = () => {
+  const { signin} = useSignIn();
 
-    const handleLogin = () => {
-        login();
-        router.push('app', 'root');
-    }
+  const handleLogin = async (values: SigninI): Promise<void> => {
+    await signin(values);
+  };
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Login</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-            <IonInput placeholder="Username" />
-        <IonInput type="password" placeholder="Password" />
-        <IonButton onClick={handleLogin}>Login</IonButton>
-            </IonContent>
-        </IonPage>
-    );
+  return (
+    <IonPage className='auth-page'>
+      <IonHeader className='ion-no-border' translucent={true}>
+        <IonToolbar color='secondary'>
+          <IonButtons slot='start'>
+            <IonBackButton defaultHref='/signup' />
+          </IonButtons>
+          <IonTitle>Log in to ChitChat</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent scrollY={false} className='auth-content ion-padding'>
+       <SignInForm onSubmit={handleLogin}/>
+      </IonContent>
+
+      <IonFooter translucent={true} className='ion-no-border'>
+              <Action message='Don’t have an account?' link='/signup' text='Sign Up'/>
+             </IonFooter>
+    </IonPage>
+  );
 };
 
 export default Login;
