c3fa85f430a946e7d1bacb1c0c7f9a27fa88275a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-26 20:52:22 +0300

    feat: Added segment functionality to the Chats component for improved UI navigation

--- src/pages/chats/Chats.tsx
+++ src/pages/chats/Chats.tsx
@@ -1,9 +1,12 @@
 import {
   IonContent,
+  IonLabel,
   IonPage,
   IonRefresher,
   IonRefresherContent,
   IonSearchbar,
+  IonSegment,
+  IonSegmentButton,
   RefresherEventDetail,
 } from '@ionic/react';
 import React, { createRef, useEffect, useRef, useState } from 'react';
@@ -18,6 +21,8 @@ import { UserItemI } from '../../shared/types';
 const Chats: React.FC<{ name: string }> = ({ name }) => {
   const contentRef = createRef<HTMLIonContentElement>();
   const [presentingElement, setPresentingElement] = useState<HTMLElement | null>(null);
+  const [activeSegment, setActiveSegment] = useState<any>('contacts');
+
   const { users, setUsers, getUsers, isLoading } = useGetUsers();
 
   let [results, setResults] = useState<UserItemI[]>([]);
@@ -36,8 +41,6 @@ const Chats: React.FC<{ name: string }> = ({ name }) => {
     setResults(users.filter((d) => d.fullName.toLowerCase().indexOf(query) > -1));
   };
 
- 
-
   useEffect(() => {
     setPresentingElement(page.current);
   }, []);
@@ -50,25 +53,44 @@ const Chats: React.FC<{ name: string }> = ({ name }) => {
 
   return (
     <IonPage ref={page} className='ion-padding'>
-      <IonSearchbar
-        showClearButton='focus'
-        clearIcon={trashBin}
-        onIonInput={handleInput}
-        type='search'
-        className='ion-margin-top'
-      />
+      <div style={{ marginTop: '24px' }}>
+        <IonSegment
+          value={activeSegment}
+          onIonChange={(e) => setActiveSegment(e.detail.value!)}
+          class='ion-color-custom-segment'
+        >
+          <IonSegmentButton value='contacts'>
+            <IonLabel>Contacts</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value='groups'>
+            <IonLabel>Groups</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+      </div>
 
-      <IonContent className='ion-padding' ref={contentRef}>
-        <IonRefresher slot='fixed' onIonRefresh={(e) => handleRefresh(e)}>
-          <IonRefresherContent
-            pullingIcon={chevronDownCircleOutline}
-            pullingText='Pull to refresh'
-            refreshingSpinner='circles'
+      {activeSegment === 'contacts' && (
+        <>
+          <IonSearchbar
+            showClearButton='focus'
+            clearIcon={trashBin}
+            onIonInput={handleInput}
+            type='search'
           />
-        </IonRefresher>
-        {isLoading && <UsersSkeleton />}
-        {!isLoading && <UsersList users={results} presentingElement={presentingElement} />}
-      </IonContent>
+
+          <IonContent className='ion-padding' ref={contentRef}>
+            <IonRefresher slot='fixed' onIonRefresh={(e) => handleRefresh(e)}>
+              <IonRefresherContent
+                pullingIcon={chevronDownCircleOutline}
+                pullingText='Pull to refresh'
+                refreshingSpinner='circles'
+              />
+            </IonRefresher>
+            {isLoading && <UsersSkeleton />}
+            {!isLoading && <UsersList users={results} presentingElement={presentingElement} />}
+          </IonContent>
+        </>
+      )}
+      {activeSegment === 'groups' && <p>Groups</p>}
     </IonPage>
   );
 };
