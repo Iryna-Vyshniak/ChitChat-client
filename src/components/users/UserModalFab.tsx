92545b0b39e540bb56337ccc8a0c22b3efb65fc6
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-04 22:31:18 +0300

    refactor: Completed UserModalFab with router direction and added data-desc

--- src/components/users/UserModalFab.tsx
+++ src/components/users/UserModalFab.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IonFab, IonFabButton, IonFabList, IonIcon } from '@ionic/react';
-import { add, callSharp, document } from 'ionicons/icons';
+import { add, callSharp, chatbubbleEllipsesOutline } from 'ionicons/icons';
 
 import { UserFabProps } from '../../shared/types';
 
@@ -20,6 +20,7 @@ const UserModalFab: React.FC<UserFabProps> = ({ user }) => {
         <IonFabList side='top'>
           <a
             href={`tel:${user?.phone}`}
+            data-desc={user?.phone ? 'Call' : 'No phone'}
             className={
               user?.phone
                 ? `custom-fab-button ios fab-button-in-list ion-activatable ion-focusable fab-button-show`
@@ -29,8 +30,12 @@ const UserModalFab: React.FC<UserFabProps> = ({ user }) => {
           >
             <IonIcon icon={callSharp} className='custom-icon'></IonIcon>
           </a>
-          <IonFabButton>
-            <IonIcon icon={document} className='custom-icon'></IonIcon>
+          <IonFabButton
+            routerLink={`/messages/send/${user?._id}`}
+            routerDirection='forward'
+            data-desc='Write message'
+          >
+            <IonIcon icon={chatbubbleEllipsesOutline} className='custom-icon'></IonIcon>
           </IonFabButton>
         </IonFabList>
       </IonFab>
