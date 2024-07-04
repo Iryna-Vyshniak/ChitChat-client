def3a1617166d87e3b39a0c858ba2e2dc15d5b3f
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-04 22:22:49 +0300

    feat: Implemented UserGroupFab component

---
+++ src/components/users/UserGroupFab.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { IonFab, IonFabButton, IonFabList, IonIcon } from '@ionic/react';
+import { add, document } from 'ionicons/icons';
+
+const UserGroupFab: React.FC = () => {
+  return (
+    <div className='user-group-fab'>
+      <IonFab slot='fixed' vertical='bottom' horizontal='end' edge>
+        <IonFabButton color='secondary' size='small'>
+          <IonIcon icon={add} color='light' />
+        </IonFabButton>
+        <IonFabList side='top'>
+          <IonFabButton
+            routerLink='/groups/create'
+            routerDirection='forward'
+            data-desc='Create Group'
+          >
+            <IonIcon icon={document} className='custom-icon'></IonIcon>
+          </IonFabButton>
+        </IonFabList>
+      </IonFab>
+    </div>
+  );
+};
+
+export default UserGroupFab;
