b5063878bcad7d2ec067a7349103f36d3cad1e48
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-05 23:57:15 +0300

    feat: Implemented ProfileFab component

---
+++ src/components/profile/ProfileFab.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { IonFab, IonFabButton, IonFabList, IonIcon } from '@ionic/react';
+import { add, person } from 'ionicons/icons';
+
+import { UserI } from '../../shared/types';
+
+const ProfileFab: React.FC<{ user: UserI | null }> = ({ user }) => {
+  return (
+    <div className='user-group-fab'>
+      <IonFab slot='fixed' vertical='bottom' horizontal='end' edge>
+        <IonFabButton color='secondary' size='small'>
+          <IonIcon icon={add} color='light' />
+        </IonFabButton>
+        <IonFabList side='top'>
+          <IonFabButton
+            routerLink={`/auth/update/${user?._id}`}
+            routerDirection='forward'
+            data-desc='Update Profile'
+          >
+            <IonIcon icon={person} className='custom-icon'></IonIcon>
+          </IonFabButton>
+        </IonFabList>
+      </IonFab>
+    </div>
+  );
+};
+
+export default ProfileFab;
