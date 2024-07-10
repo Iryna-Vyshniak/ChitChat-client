fa7f0f8530785d9e3bede075e95f6881073cbee4
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-11 02:37:33 +0300

    feat(data): Added tags and postFields for post creation form
    - Defined an array of tags for categorizing posts.
      - Tags include: culture, animal, future, nature, fashion, sport, politics, travel, health, food, beauty, events, science, technology, humanitarianAid, mission, peace, life, savingLives, work, psychology, kids.
    
    - Added postFields object to define form fields for post creation.
      - title: includes name, placeholder, and label.
      - tags: includes name, placeholder, and label.
      - text: includes name, placeholder, and label.
      - imageUrl: includes name, placeholder, and label.
    
    This update provides necessary data for rendering and managing the post creation form.

--- src/shared/data/index.tsx
+++ src/shared/data/index.tsx
@@ -1,116 +1,151 @@
-import { archiveOutline, archiveSharp, earthOutline, heartOutline, heartSharp, idCardOutline, notificationsOutline, paperPlaneOutline, paperPlaneSharp, personCircleOutline, settingsOutline } from "ionicons/icons";
+import {
+  archiveOutline,
+  archiveSharp,
+  earthOutline,
+  heartOutline,
+  heartSharp,
+  idCardOutline,
+  notificationsOutline,
+  paperPlaneOutline,
+  paperPlaneSharp,
+  personCircleOutline,
+  settingsOutline,
+} from 'ionicons/icons';
 
-import { AppPage } from "../types";
+import { AppPage } from '../types';
 
 export const appPages: AppPage[] = [
-    {
-      title: 'Profile',
-      url: '/app/Profile',
-      iosIcon: personCircleOutline,
-      mdIcon: personCircleOutline,
-    },
-    {
-      title: 'Posts',
-      url: '/app/Posts',
-      iosIcon: idCardOutline,
-      mdIcon: idCardOutline,
-    },
-    {
-      title: 'Chats',
-      url: '/app/Chats',
-      iosIcon: paperPlaneOutline,
-      mdIcon: paperPlaneSharp,
-    },
-    {
-      title: 'Notifications',
-      url: '/app/Notifications',
-      iosIcon: notificationsOutline,
-      mdIcon:  notificationsOutline,
-    },
-    {
-      title: 'Favorites',
-      url: '/app/Favorites',
-      iosIcon: heartOutline,
-      mdIcon: heartSharp,
-    },
-    {
-      title: 'Location',
-      url: '/app/Location',
-      iosIcon: earthOutline,
-      mdIcon: earthOutline,
-    },
-    {
-      title: 'Archived',
-      url: '/app/Archived',
-      iosIcon: archiveOutline,
-      mdIcon: archiveSharp,
-    },
-    {
-      title: 'Settings',
-      url: '/app/Settings',
-      iosIcon: settingsOutline,
-      mdIcon: settingsOutline,
-    },
-  ];
+  {
+    title: 'Profile',
+    url: '/app/Profile',
+    iosIcon: personCircleOutline,
+    mdIcon: personCircleOutline,
+  },
+  {
+    title: 'Posts',
+    url: '/app/Posts',
+    iosIcon: idCardOutline,
+    mdIcon: idCardOutline,
+  },
+  {
+    title: 'Chats',
+    url: '/app/Chats',
+    iosIcon: paperPlaneOutline,
+    mdIcon: paperPlaneSharp,
+  },
+  {
+    title: 'Notifications',
+    url: '/app/Notifications',
+    iosIcon: notificationsOutline,
+    mdIcon: notificationsOutline,
+  },
+  {
+    title: 'Favorites',
+    url: '/app/Favorites',
+    iosIcon: heartOutline,
+    mdIcon: heartSharp,
+  },
+  {
+    title: 'Location',
+    url: '/app/Location',
+    iosIcon: earthOutline,
+    mdIcon: earthOutline,
+  },
+  {
+    title: 'Archived',
+    url: '/app/Archived',
+    iosIcon: archiveOutline,
+    mdIcon: archiveSharp,
+  },
+  {
+    title: 'Settings',
+    url: '/app/Settings',
+    iosIcon: settingsOutline,
+    mdIcon: settingsOutline,
+  },
+];
 
 export const appMobilePages: AppPage[] = [
-    {
-      title: 'Profile',
-      url: '/app/Profile',
-      iosIcon: personCircleOutline,
-      mdIcon: personCircleOutline,
-    },
-    {
-      title: 'Location',
-      url: '/app/Location',
-      iosIcon: earthOutline,
-      mdIcon: earthOutline,
-    },
-    {
-      title: 'Archived',
-      url: '/app/Archived',
-      iosIcon: archiveOutline,
-      mdIcon: archiveSharp,
-    },
-    {
-      title: 'Settings',
-      url: '/app/Settings',
-      iosIcon: settingsOutline,
-      mdIcon: settingsOutline,
-    },
-  ];
-
+  {
+    title: 'Profile',
+    url: '/app/Profile',
+    iosIcon: personCircleOutline,
+    mdIcon: personCircleOutline,
+  },
+  {
+    title: 'Location',
+    url: '/app/Location',
+    iosIcon: earthOutline,
+    mdIcon: earthOutline,
+  },
+  {
+    title: 'Archived',
+    url: '/app/Archived',
+    iosIcon: archiveOutline,
+    mdIcon: archiveSharp,
+  },
+  {
+    title: 'Settings',
+    url: '/app/Settings',
+    iosIcon: settingsOutline,
+    mdIcon: settingsOutline,
+  },
+];
 
 export const appTabs: AppPage[] = [
-    {
-      title: 'Posts',
-      url: '/app/Posts',
-      iosIcon: idCardOutline,
-      mdIcon: idCardOutline,
-    },
-    {
-      title: 'Chats',
-      url: '/app/Chats',
-      iosIcon: paperPlaneOutline,
-      mdIcon: paperPlaneSharp,
-    },
-    {
-      title: 'Notifications',
-      url: '/app/Notifications',
-      iosIcon: notificationsOutline,
-      mdIcon:  notificationsOutline,
-    },
-    {
-      title: 'Favorites',
-      url: '/app/Favorites',
-      iosIcon: heartOutline,
-      mdIcon: heartSharp,
-    },
-  ];
+  {
+    title: 'Posts',
+    url: '/app/Posts',
+    iosIcon: idCardOutline,
+    mdIcon: idCardOutline,
+  },
+  {
+    title: 'Chats',
+    url: '/app/Chats',
+    iosIcon: paperPlaneOutline,
+    mdIcon: paperPlaneSharp,
+  },
+  {
+    title: 'Notifications',
+    url: '/app/Notifications',
+    iosIcon: notificationsOutline,
+    mdIcon: notificationsOutline,
+  },
+  {
+    title: 'Favorites',
+    url: '/app/Favorites',
+    iosIcon: heartOutline,
+    mdIcon: heartSharp,
+  },
+];
 
-  export const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+export const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+export const tags = [
+  'culture',
+  'animal',
+  'future',
+  'nature',
+  'fashion',
+  'sport',
+  'politics',
+  'travel',
+  'health',
+  'food',
+  'beauty',
+  'events',
+  'science',
+  'technology',
+  'humanitarianAid',
+  'mission',
+  'peace',
+  'life',
+  'savingLives',
+  'work',
+  'psychology',
+  'kids',
+];
 
-  // for signup form
+// for signup form
 export const signupFields = {
   fullName: {
     label: 'Full Name',
@@ -154,5 +189,28 @@ export const loginFields = {
     label: 'Password',
     name: 'password',
     placeholder: '*******',
+  },
+};
+
+export const postFields = {
+  title: {
+    name: 'title',
+    placeholder: 'Enter a title...',
+    label: 'Title',
+  },
+  tags: {
+    name: 'tags',
+    placeholder: 'Select a category',
+    label: 'Category',
+  },
+  text: {
+    name: 'text',
+    placeholder: 'Enter a description...',
+    label: 'Description',
+  },
+  imageUrl: {
+    name: 'imageUrl',
+    placeholder: 'Add image...',
+    label: 'Image',
   },
 };
