54a8361bdd44dd951f9192fcd32f128fc6965e71
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-10 23:45:00 +0300

    feat: Implemented PostsFab component

---
+++ src/components/posts/PostsFab.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { IonFab, IonFabButton, IonFabList, IonIcon } from '@ionic/react';
+import { add, newspaperOutline } from 'ionicons/icons';
+
+const PostsFab: React.FC = () => {
+  return (
+    <div className='user-group-fab'>
+      <IonFab slot='fixed' vertical='bottom' horizontal='end' edge>
+        <IonFabButton color='secondary' size='small'>
+          <IonIcon icon={add} color='light' />
+        </IonFabButton>
+        <IonFabList side='top'>
+          <IonFabButton
+            routerLink={`/posts/create`}
+            routerDirection='forward'
+            data-desc='Upload Post'
+          >
+            <IonIcon icon={newspaperOutline} className='custom-icon'></IonIcon>
+          </IonFabButton>
+        </IonFabList>
+      </IonFab>
+    </div>
+  );
+};
+
+export default PostsFab;
