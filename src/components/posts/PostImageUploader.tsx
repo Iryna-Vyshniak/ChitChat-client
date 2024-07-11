b498594a1f6e2c424a8f761f2b8eb4b4af2f4e13
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-12 01:02:44 +0300

    feat(PostImageUploader): Implemented image uploader component
    - Imported necessary modules from @ionic/react, ionicons/icons, and React.
    - Defined the PostImageUploader component to handle image uploading.
    - Utilized useRef to reference the file input element.
    - Added handleClick function to trigger file input click event.
    - Rendered input field for file selection with associated handlers for change and blur events.
    - Displayed thumbnail of the selected image if imageUrl is provided.
    - Rendered upload prompt with IonIcon and IonButton if no image is selected.
    - Displayed error message if imageUrl is invalid and the field is touched.
    
    This update provides a user-friendly component for uploading images within the post creation flow.

---
+++ src/components/posts/PostImageUploader.tsx
@@ -0,0 +1,64 @@
+import { IonButton, IonIcon, IonText } from '@ionic/react';
+import { cloudCircleOutline } from 'ionicons/icons';
+import React, { useRef } from 'react';
+
+import './PostImageUploader.css';
+
+import { PostImageUploaderProps } from '../../shared/types';
+import { postFields } from '../../shared/data';
+
+const PostImageUploader: React.FC<PostImageUploaderProps> = ({
+  imageUrl,
+  handleImageChange,
+  markTouched,
+  touchedFields,
+  errors,
+}) => {
+  const fileRef = useRef<HTMLInputElement | null>(null);
+
+  const handleClick = () => {
+    if (fileRef.current) {
+      fileRef.current.click();
+    }
+  };
+
+  return (
+    <div className='post-img-container'>
+      <input
+        type='file'
+        className='post-img-input'
+        ref={fileRef}
+        onChange={(event) => handleImageChange(event)}
+        onBlur={() => markTouched('imageUrl')}
+        {...postFields.imageUrl}
+      />
+      {imageUrl ? (
+        <div className='post-img-thumb'>
+          <img
+            src={imageUrl}
+            alt='post image'
+            width={250}
+            height={250}
+            className='full-width full-height cover-center'
+          />
+        </div>
+      ) : (
+        <div className='post-uploader'>
+          <IonIcon icon={cloudCircleOutline} className='icon-uploader' />
+          <h3>Drag photo here</h3>
+          <p>SVG, PNG, JPG</p>
+          <IonButton color='intro-violet' expand='block' onClick={handleClick}>
+            Select from your device
+          </IonButton>
+        </div>
+      )}
+      {errors.imageUrl && touchedFields.imageUrl && !imageUrl && (
+        <IonText color='danger' className='ion-padding-start'>
+          <p className='select-error error-text'>{errors.imageUrl}</p>
+        </IonText>
+      )}
+    </div>
+  );
+};
+
+export default PostImageUploader;
