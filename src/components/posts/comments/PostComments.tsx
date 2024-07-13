5d2c175d9356c43ea8ce3a5de992a40324b6769b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-13 14:27:11 +0300

    feat(PostComments): Implemented PostComments component

---
+++ src/components/posts/comments/PostComments.tsx
@@ -0,0 +1,26 @@
+import { IonAccordion, IonAccordionGroup, IonItem, IonLabel } from '@ionic/react';
+import React from 'react';
+
+import PostComment from './PostComment';
+
+const PostComments: React.FC = () => {
+  return (
+    <div className='post-comments'>
+      <IonAccordionGroup>
+        <IonAccordion value='first' toggleIcon={''} toggleIconSlot='start'>
+          <IonItem slot='header'>
+            <IonLabel>
+              <p className='post-comments-header'>View all comments</p>
+            </IonLabel>
+          </IonItem>
+
+          <ul className='ion-padding ion-no-margin' slot='content'>
+            <PostComment />
+          </ul>
+        </IonAccordion>
+      </IonAccordionGroup>
+    </div>
+  );
+};
+
+export default PostComments;
