387a72cd6abfa9e10b63ac629fc36cae2da601d0
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-13 14:32:39 +0300

    feat(PostPage): Implemented PostsPage component with post fetching and refresh functionality
    - Introduced PostsPage component using React and Ionic
    - Included IonPage, IonContent, IonRefresher, and IonRefresherContent for page structure and refresh functionality
    - Integrated useGetPosts custom hook for fetching posts
    - Implemented handleRefresh function to refresh posts list on pull-to-refresh action
    - Added PostsList component to display fetched posts
    - Added PostsFab component for floating action button
    - Managed loading state to conditionally render posts list
    - Applied custom padding and margin styles for layout

---
+++ src/pages/posts/PostsPage.tsx
@@ -0,0 +1,51 @@
+import {
+  IonContent,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  RefresherEventDetail,
+} from '@ionic/react';
+import { chevronDownCircleOutline } from 'ionicons/icons';
+import React, { createRef, useEffect, useRef, useState } from 'react';
+
+import PostsFab from '../../components/posts/PostsFab';
+import PostsList from '../../components/posts/PostsLists';
+
+import { useGetPosts } from '../../shared/hooks/post/useGetPosts';
+
+const PostsPage: React.FC = () => {
+  const [presentingElement, setPresentingElement] = useState<HTMLElement | null>(null);
+  const { posts, setPosts, getPosts, isLoading } = useGetPosts();
+
+  const contentRef = createRef<HTMLIonContentElement>();
+  const page = useRef(null);
+
+  useEffect(() => {
+    setPresentingElement(page.current);
+  }, []);
+
+  const handleRefresh = async (event: CustomEvent<RefresherEventDetail>) => {
+    const posts = await getPosts();
+    setPosts(posts);
+    event.detail.complete();
+  };
+
+  return (
+    <IonPage ref={page} className='ion-padding' style={{ marginTop: '24px' }}>
+      <IonContent ref={contentRef} className='ion-padding'>
+        <IonRefresher slot='fixed' onIonRefresh={(e) => handleRefresh(e)}>
+          <IonRefresherContent
+            pullingIcon={chevronDownCircleOutline}
+            pullingText='Pull to refresh'
+            refreshingSpinner='circles'
+          />
+        </IonRefresher>
+        {!isLoading && posts.length > 0 && <PostsList posts={posts} />}
+
+        <PostsFab />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default PostsPage;
