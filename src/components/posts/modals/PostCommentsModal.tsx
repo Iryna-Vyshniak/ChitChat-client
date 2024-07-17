dedd03f137524e56d5ae3ce61a5aad50df3b6c0e
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-17 18:39:43 +0300

    feat: Implemented PostCommentsModal component

---
+++ src/components/posts/modals/PostCommentsModal.tsx
@@ -0,0 +1,42 @@
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonIcon,
+  IonModal,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import { closeOutline } from 'ionicons/icons';
+
+import PostComment from '../comments/PostComment';
+import { PostCommentsModalProps } from '../../../shared/types';
+
+const PostCommentsModal: React.FC<PostCommentsModalProps> = ({
+  presentingElement,
+  modalAllComments,
+}) => {
+  function dismiss() {
+    modalAllComments.current?.dismiss();
+  }
+
+  return (
+    <IonModal ref={modalAllComments} presentingElement={presentingElement!}>
+      <IonContent>
+        <IonToolbar>
+          <IonTitle>All comments to post</IonTitle>
+          <IonButtons slot='end'>
+            <IonButton onClick={() => dismiss()}>
+              <IonIcon icon={closeOutline} slot='end' className='custom-icon' />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+        <ul className='ion-padding ion-margin'>
+          <PostComment />
+        </ul>
+      </IonContent>
+    </IonModal>
+  );
+};
+
+export default PostCommentsModal;
