9c954d4f36d8db163535936bc6a18ff5dd2b0635
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-21 20:20:05 +0300

    feat: Add utility functions for downloading images from Cloudinary
    - Created `downloadImage` function to generate a download link for images from Cloudinary.
    - Implemented `getFileNameFromUrl` function to extract the file name from a given URL.
    - Added `downloadImagePost` function to create and trigger a download link in the browser.

--- src/shared/utils/index.tsx
+++ src/shared/utils/index.tsx
@@ -1,13 +1,32 @@
 //  A function that returns a date in the format "10 Mar 2024"
 
 export const getFormattedDate = (date: string) => {
-  if (!date) return "No date";
+  if (!date) return 'No date';
 
-    const options: Intl.DateTimeFormatOptions = {
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric',
-    };
-
-    return new Date(date).toLocaleDateString('en-US', options);
+  const options: Intl.DateTimeFormatOptions = {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
   };
+
+  return new Date(date).toLocaleDateString('en-US', options);
+};
+
+// create link to download image from cloudinary
+export const downloadImage = (url: string) => {
+  return `${url.split('/upload/')[0]}/upload/fl_attachment/${url.split('/upload/')[1]}`;
+};
+
+export const getFileNameFromUrl = (url: string) => {
+  const parts = url.split('/');
+  return parts[parts.length - 1];
+};
+
+export const downloadImagePost = (url: string) => {
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', getFileNameFromUrl(url));
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+};
