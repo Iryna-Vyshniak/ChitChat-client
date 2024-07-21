a054f9567c2873fc3c356e9d8b17ca874febc174
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-21 20:40:21 +0300

    refactor: Refactored PostCard component
    - Updated useState type for like state from Boolean to boolean.
    - Added PostImage component to handle post image rendering and like functionality.
    - Introduced IonItemSliding for sliding item effect in post card.
    - Added post-stats section to display view count using IonIcon and IonLabel.
    - Improved the structure and readability of the JSX code.
    - Ensured consistent import and use of types and components.

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -4,6 +4,8 @@ import {
   IonCard,
   IonCardContent,
   IonIcon,
+  IonItemSliding,
+  IonLabel,
   IonRouterLink,
 } from '@ionic/react';
 import {
@@ -13,6 +15,7 @@ import {
   paperPlaneOutline,
   bookmarkOutline,
   heartOutline,
+  statsChartSharp,
 } from 'ionicons/icons';
 
 import './PostCard.css';
@@ -22,11 +25,12 @@ import { PostCardI } from '../../shared/types';
 import PostComments from './comments/PostComments';
 import PostAddComment from './comments/PostAddComment';
 import PostCommentsModal from './modals/PostCommentsModal';
+import PostImage from './PostImage';
 
 const PostCard: React.FC<{ post: PostCardI }> = ({
   post: { _id, imageUrl, likedBy, owner, tags, text, title, viewsCount, createdAt },
 }) => {
-  const [like, setLike] = useState<Boolean>(false);
+  const [like, setLike] = useState<boolean>(false);
   const content = useRef(null);
   const modalAllComments = useRef<HTMLIonModalElement | null>(null);
 
@@ -48,91 +52,84 @@ const PostCard: React.FC<{ post: PostCardI }> = ({
 
   return (
     <>
-      {' '}
       <IonCard className='post-container ion-padding custom-card' ref={content}>
-        <IonCardContent className='post-inner-container ion-no-padding'>
-          <div className='post-profile'>
-            <div className='post-profile-info'>
-              <IonRouterLink routerLink={`/profile/${owner._id}`}>
-                <IonAvatar>
-                  <img alt='post avatar' src={owner.avatar} width={44} height={44} />
-                </IonAvatar>
-              </IonRouterLink>
+        <IonItemSliding>
+          <IonCardContent className='post-inner-container ion-no-padding'>
+            <div className='post-profile'>
+              <div className='post-profile-info'>
+                <IonRouterLink routerLink={`/profile/${owner._id}`}>
+                  <IonAvatar>
+                    <img alt='post avatar' src={owner.avatar} width={44} height={44} />
+                  </IonAvatar>
+                </IonRouterLink>
 
-              <IonRouterLink routerLink={`/profile/${owner._id}`}>
-                <p>{owner.fullName}</p>
-              </IonRouterLink>
+                <IonRouterLink routerLink={`/profile/${owner._id}`}>
+                  <p>{owner.fullName}</p>
+                </IonRouterLink>
+              </div>
+
+              <div className='post-profile-more'>
+                <IonIcon icon={ellipsisVertical} />
+              </div>
+            </div>
+            <PostImage image={imageUrl} like={like} handleLikeClick={handleLikeClick} />
+
+            <div className='post-actions-container'>
+              <div className='post-actions'>
+                <button className='btn-action'>
+                  {' '}
+                  <IonIcon
+                    className='animate__animated'
+                    color={!like ? '' : 'danger'}
+                    icon={!like ? heartOutline : heart}
+                    onClick={handleLikeClick}
+                  />
+                </button>
+
+                <button className='btn-action' onClick={openModal}>
+                  <IonIcon icon={chatbubbleOutline} />
+                </button>
+
+                <button className='btn-action'>
+                  <IonIcon icon={paperPlaneOutline} />
+                </button>
+              </div>
+
+              <div className='post-stats'>
+                <IonIcon icon={statsChartSharp} />
+                <IonLabel>{viewsCount}</IonLabel>
+              </div>
+
+              <div className='post-bookmark'>
+                <IonIcon icon={bookmarkOutline} />
+              </div>
             </div>
 
-            <div className='post-profile-more'>
-              <IonIcon icon={ellipsisVertical} />
-            </div>
-          </div>
-
-          <div
-            className='post-image'
-            style={{
-              backgroundImage: `url(${imageUrl})`,
-              backgroundPosition: 'center, center',
-              backgroundSize: 'cover',
-              backgroundColor: 'var(--ion-color-step-650)',
-            }}
-          >
-            <IonIcon
-              className='animated__animated animate__heartBeat post-image-like'
-              icon={!like ? heartOutline : heart}
-              color={!like ? 'intro-rosy' : 'danger'}
-              onClick={handleLikeClick}
-            />
-          </div>
-
-          <div className='post-actions-container'>
-            <div className='post-actions'>
-              <button className='btn-action'>
-                {' '}
-                <IonIcon
-                  className='animate__animated'
-                  color={!like ? '' : 'danger'}
-                  icon={!like ? heartOutline : heart}
-                  onClick={handleLikeClick}
-                />
-              </button>
-
-              <button className='btn-action' onClick={openModal}>
-                <IonIcon icon={chatbubbleOutline} />
-              </button>
-
-              <button className='btn-action'>
-                <IonIcon icon={paperPlaneOutline} />
-              </button>
+            <div className='post-likes-container'>
+              <p>
+                Liked by <span className='post-liked-name'>Dillan</span> and{' '}
+                <span className='post-liked-name'>2 others</span>
+              </p>
             </div>
 
-            <div className='post-bookmark'>
-              <IonIcon icon={bookmarkOutline} />
+            <div className='post-title'>
+              <p>
+                <span className='post-name'>
+                  <IonRouterLink routerLink={`/profile/${owner._id}`}>
+                    {owner.fullName}
+                  </IonRouterLink>
+                </span>{' '}
+                {title}
+              </p>
+
+              <PostComments />
+
+              <PostAddComment date={createdAt} />
             </div>
-          </div>
+          </IonCardContent>
+        </IonItemSliding>
+      </IonCard>
 
-          <div className='post-likes-container'>
-            <p>
-              Liked by <span className='post-liked-name'>Dillan</span> and{' '}
-              <span className='post-liked-name'>2 others</span>
-            </p>
-          </div>
-
-          <div className='post-title'>
-            <p>
-              <span className='post-name'>
-                <IonRouterLink routerLink={`/profile/${owner._id}`}>{owner.fullName}</IonRouterLink>
-              </span>{' '}
-              {title}
-            </p>
-
-            <PostComments />
-
-            <PostAddComment date={createdAt} />
-          </div>
-        </IonCardContent>
-      </IonCard>{' '}
       <PostCommentsModal
         presentingElement={presentingElement}
         modalAllComments={modalAllComments}
