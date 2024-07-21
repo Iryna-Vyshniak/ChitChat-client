80cb446537feaec4d7405e2ad19901fb7901cda4
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-21 14:16:22 +0300

    feat: Added color definitions for intro theme

--- src/shared/constants/index.tsx
+++ src/shared/constants/index.tsx
@@ -1,5 +1,7 @@
 export const API = import.meta.env.VITE_API_KEY;
 
 export const regExp = {
-    email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-  };
+  email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+};
+
+export const colors = ['intro-green', 'intro-violet', 'intro-rosy', 'intro-yellow', 'intro-blue'];
