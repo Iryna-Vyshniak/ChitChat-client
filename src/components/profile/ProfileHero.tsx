7c37278cf3d089fe1700ce6c881c1b8032216b9f
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-28 08:31:33 +0300

    feat: Implement ProfileHero component with loading state and user data integration
    - Created `ProfileHero` component to display user cover and avatar.
    - Integrated `useUserInfo` hook to fetch and display user data based on `userId`.
    - Added loading state handling with `ProfileHeroSkeleton` for smooth UX.
    - Implemented error handling for cases where user information fails to load.
    - Applied responsive styles for cover image and avatar positioning.

---
+++ src/components/profile/ProfileHero.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { IonImg } from '@ionic/react';
+
+import { useUserInfo } from '../../shared/hooks/users/useUserInfo';
+import Avatar from '../avatar/Avatar';
+import './ProfileHero.css';
+import ProfileHeroSkeleton from './ProfileHeroSkeleton';
+
+interface ProfileHeroProps {
+  userId: string;
+}
+
+const ProfileHero: React.FC<ProfileHeroProps> = ({ userId }) => {
+  const { userInfo, isUserInfoLoading } = useUserInfo(userId);
+
+  if (isUserInfoLoading && !userInfo) {
+    return <ProfileHeroSkeleton />;
+  }
+
+  if (!userInfo) {
+    // Handle the case where userInfo is still undefined even after loading
+    return <div>Error loading user information.</div>;
+  }
+
+  const { cover, avatar } = userInfo;
+
+  return (
+    <div className='profile-hero'>
+      <div className='profile-cover-wrapper'>
+        {cover && (
+          <IonImg src={cover} alt='Cover Image' className='profile-cover' />
+        )}
+      </div>
+
+      <div className='avatar-position'>
+        <Avatar avatar={avatar} isLarge hasBorder />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileHero;
