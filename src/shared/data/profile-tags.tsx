989d1b12758ff44384a692fc54b832a312a87520
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-30 15:24:01 +0300

    feat: Created profile tags data
    - Added `data/profile-tags.tsx` to store predefined profile tags.
    - The tags include `followings`, `followers`, and `posts`, each with a unique `id` and `label`.
    - This will be used to manage and display user profile sections efficiently.

---
+++ src/shared/data/profile-tags.tsx
@@ -0,0 +1,14 @@
+export const PROFILE_TAGS = [
+  {
+    id: '73ehdajla',
+    label: 'followings',
+  },
+  {
+    id: '43fudewhdbs',
+    label: 'followers',
+  },
+  {
+    id: '8453refjs',
+    label: 'posts',
+  },
+];
