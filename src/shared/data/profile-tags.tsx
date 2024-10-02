159621efe66161651747a9e167c6f7de5e62021c
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-10-02 20:35:39 +0300

    refactor(types): Added `as const` for profile tags
    - Added `as const` to the `label` fields in `PROFILE_TAGS` array.
    - Ensures that TypeScript treats `label` values as literal types ('followings', 'followers', 'posts') instead of generic `string` type.
    - Improves type safety and enforces stricter typings for label usage across the application.

--- src/shared/data/profile-tags.tsx
+++ src/shared/data/profile-tags.tsx
@@ -1,14 +1,16 @@
 export const PROFILE_TAGS = [
   {
     id: '73ehdajla',
-    label: 'followings',
+    label: 'followings' as const,
   },
   {
     id: '43fudewhdbs',
-    label: 'followers',
+    label: 'followers' as const,
   },
   {
     id: '8453refjs',
-    label: 'posts',
+    label: 'posts' as const,
   },
 ];
+
+//  використання as const, яке дозволяє TypeScript розуміти, що значення label є конкретними літеральними рядками, а не просто рядками типу string.
