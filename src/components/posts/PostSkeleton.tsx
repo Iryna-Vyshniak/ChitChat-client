401f16ddcbac9e5754c1eb48728eeca4293ef7d2
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-10-05 21:20:08 +0300

    refactor: Modularized PostsSkeleton by creating PostSkeleton component
    - Created `PostSkeleton` component to modularize the skeleton structure for a single post.
    - Updated `PostsSkeleton` component to render `PostSkeleton` instances instead of duplicating the post skeleton structure.
    - Improved code maintainability and reusability by separating the logic for individual post skeletons.
    - Maintained the same layout and styling for post skeletons, ensuring no changes in appearance.

---
+++ src/components/posts/PostSkeleton.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import {
+  IonCard,
+  IonContent,
+  IonLabel,
+  IonSkeletonText,
+  IonThumbnail,
+} from '@ionic/react';
+
+const PostSkeleton: React.FC = () => {
+  return (
+    <IonContent className='custom-content ion-padding'>
+      <IonCard className='post-container ion-padding ion-margin'>
+        <div className='post-inner-container'>
+          <div className='post-profile'>
+            <div className='post-profile-info'>
+              <IonThumbnail slot='start'>
+                <IonSkeletonText animated={true} />
+              </IonThumbnail>
+
+              <IonSkeletonText
+                animated={true}
+                style={{ marginLeft: '0.2rem', width: '120px' }}
+              />
+            </div>
+
+            <div className='post-profile-more'>
+              <IonSkeletonText
+                animated={true}
+                style={{ width: '10px', height: '24px' }}
+              />
+            </div>
+          </div>
+
+          <IonThumbnail className='post-image post-image-skeleton'>
+            <IonSkeletonText animated={true} />
+          </IonThumbnail>
+
+          <div className='post-actions-container'>
+            <div className='post-actions'>
+              <IonSkeletonText
+                animated={true}
+                style={{ width: '24px', height: '24px' }}
+              />
+              <IonSkeletonText
+                animated={true}
+                style={{ width: '24px', height: '24px' }}
+              />
+              <IonSkeletonText
+                animated={true}
+                style={{ width: '24px', height: '24px' }}
+              />
+            </div>
+            <IonLabel className='post-bookmark'>
+              <IonSkeletonText
+                animated={true}
+                style={{ width: '20px', height: '24px' }}
+              />
+            </IonLabel>
+          </div>
+        </div>
+        <div className='post-likes-container'>
+          <p>
+            <IonSkeletonText animated={true} style={{ width: '50%' }} />
+          </p>
+        </div>
+
+        <div className='post-title'>
+          <p className='flex-start'>
+            <IonSkeletonText animated={true} style={{ width: '25%' }} />
+            <IonSkeletonText animated={true} style={{ width: '10%' }} />
+          </p>
+        </div>
+      </IonCard>
+    </IonContent>
+  );
+};
+
+export default PostSkeleton;
