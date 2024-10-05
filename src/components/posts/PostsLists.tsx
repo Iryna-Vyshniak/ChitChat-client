98c66f9449ccd34c7d3d6b567c16a7176f58dad9
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-10-05 21:39:01 +0300

    refactor: Updated PostsList to add back-to-top functionality and remove PostPopup
    - Replaced `PostPopup` modal with a `back-to-top` feature using `IonFab` and `IonFabButton` for smoother navigation.
    - Added `useScrollToAnchor` hook to enable scrolling to specific posts using URL hashes.
    - Introduced scrolling logic to show or hide the `back-to-top` button when the user scrolls down.
    - Removed `AnimatePresence` and modal-related functionality for a simpler and cleaner layout.
    - Improved user experience by allowing quick navigation back to the top of the posts list.

--- src/components/posts/PostsLists.tsx
+++ src/components/posts/PostsLists.tsx
@@ -1,42 +1,78 @@
-import React, { useState } from 'react';
+import React, { createRef, useState } from 'react';
 
-import { IonContent, IonItem, IonList } from '@ionic/react';
-import { AnimatePresence } from 'framer-motion';
+import {
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonItem,
+  IonList,
+} from '@ionic/react';
+import { chevronUp } from 'ionicons/icons';
 
+import { useScrollToAnchor } from '../../shared/hooks/scroll/useScrollToAnchor';
 import { PostCardI } from '../../shared/types';
 import PostCard from './PostCard';
-import PostPopup from './PostPopup';
 
 const PostsList: React.FC<{ posts: PostCardI[] }> = ({ posts }) => {
-  const [selectedPost, setSelectedPost] = useState<PostCardI | undefined>(
-    undefined
-  );
+  const [, setSelectedPost] = useState<PostCardI | undefined>(undefined);
+  const [backToTop, setBackToTop] = useState<boolean>(false);
+  const contentRef = createRef<HTMLIonContentElement>();
+  useScrollToAnchor();
+
+  const goToTop = () => {
+    if (contentRef.current) {
+      contentRef.current.scrollToTop(1000); // Scroll to the top in 1000ms
+    }
+  };
+
+  const scrollToTop = (event: CustomEvent) => {
+    const scrollTop = event.detail.scrollTop;
+    const threshold = 50;
+
+    if (scrollTop > threshold) {
+      setBackToTop(true);
+    } else {
+      setBackToTop(false);
+    }
+  };
+
+  const handleContentScroll = (event: CustomEvent) => {
+    scrollToTop(event);
+  };
+
   return (
-    <IonContent className='ion-margin-vertical'>
-      <div className='content-container'>
-        <IonList>
-          {' '}
-          {posts.map((post) => (
-            <IonItem
-              key={'card-' + post._id}
-              lines='none'
-              className='ion-no-padding ion-no-inner-padding'
-            >
-              {' '}
-              <PostCard
-                post={post}
-                selectPost={setSelectedPost}
-                selectedPost={selectedPost}
-              />
-            </IonItem>
-          ))}
-        </IonList>
-        <AnimatePresence>
-          {selectedPost && (
-            <PostPopup post={selectedPost} closePopup={setSelectedPost} />
-          )}
-        </AnimatePresence>
-      </div>
+    <IonContent
+      className='ion-margin-vertical custom-content'
+      ref={contentRef}
+      scrollEvents={true}
+      fixedSlotPlacement='before'
+      onIonScroll={(e) => handleContentScroll(e)}
+    >
+      <IonList>
+        {posts.map((post) => (
+          <IonItem
+            key={'card-' + post._id}
+            lines='none'
+            className='ion-no-padding ion-no-inner-padding'
+          >
+            {' '}
+            <PostCard post={post} selectPost={setSelectedPost} />
+          </IonItem>
+        ))}
+      </IonList>
+      {backToTop && (
+        <IonFab
+          horizontal='center'
+          vertical='bottom'
+          slot='fixed'
+          onClick={goToTop}
+        >
+          <IonFabButton className='custom-fab-button fab-scroll'>
+            <IonIcon icon={chevronUp}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      )}
     </IonContent>
   );
 };
