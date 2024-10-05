370833b3a82df54ecd9716df7930f9891a9ac717
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-10-05 21:52:27 +0300

    feat(UsersList): Added variant and isAuthProfile props for enhanced user handling
    - Added `variant` prop to `UsersList` for differentiating between 'followers', 'followings', and 'chat' views.
    - Introduced `isAuthProfile` prop to control actions available for authenticated users.
    - Implemented conditional rendering for modals and actions based on user selection and profile type.
    - Improved user experience with customized messages when no users are available for the selected variant.
    - Refined user interactions by passing `onAction` prop to handle actions like following or unfollowing.

--- src/components/users/UsersList.tsx
+++ src/components/users/UsersList.tsx
@@ -1,32 +1,50 @@
 import React, { useState } from 'react';
 
-import { IonCard, IonContent } from '@ionic/react';
+import { IonCard, IonContent, IonTitle } from '@ionic/react';
 
 import { UserItemI, UserListProps } from '../../shared/types';
 import UserCard from './UserCard';
 import UserModal from './UserModal';
 
-const UsersList: React.FC<UserListProps> = ({ users, presentingElement }) => {
+const UsersList: React.FC<UserListProps> = ({
+  users,
+  presentingElement,
+  variant,
+  isAuthProfile,
+  onAction,
+}) => {
   const [selectedUser, setSelectedUser] = useState<UserItemI | null>(null);
-
   return (
     <IonContent className='custom-content ion-padding'>
-      {users.map((user) => (
-        <IonCard
-          key={user._id}
-          onClick={() => {
-            setSelectedUser(user);
-          }}
-          className='custom-card'
-        >
-          <UserCard user={user} />
-        </IonCard>
-      ))}
-      <UserModal
-        selectedUser={selectedUser}
-        setSelectedUser={setSelectedUser}
-        presentingElement={presentingElement}
-      />
+      {users.length > 0 ? (
+        <>
+          {users.map((user) => (
+            <IonCard
+              key={user._id}
+              onClick={() => {
+                setSelectedUser(user);
+              }}
+              className='custom-card'
+            >
+              <UserCard
+                user={user}
+                variant={variant}
+                isAuthProfile={isAuthProfile}
+                onAction={onAction}
+              />
+            </IonCard>
+          ))}
+          {presentingElement && (
+            <UserModal
+              selectedUser={selectedUser}
+              setSelectedUser={setSelectedUser}
+              presentingElement={presentingElement}
+            />
+          )}{' '}
+        </>
+      ) : (
+        <IonTitle>{`Not ${variant} yet`}</IonTitle>
+      )}
     </IonContent>
   );
 };
