235ab5bffd2190b37c3ef2068ba62699a712e1ac
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-10-05 21:50:20 +0300

    feat(ProfileCard): Added dynamic post and user details rendering
    - Added dynamic rendering for posts, followers, and followings with `activeDetails` state to toggle between views.
    - Integrated `usePostStore` to fetch posts and display user's posts in `ProfilePosts` component.
    - Improved data handling by fetching followers and followings using `useUserStore` and filtering undefined values.
    - Enhanced follow/unfollow functionality with `useFollowUnfollow` to update follow state and counts.
    - Displayed relevant skeleton components (`ProfilePostsSkeleton`, `ProfileCardSkeleton`) during loading states.

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { IonContent, IonIcon, IonItem, IonLabel, IonList } from '@ionic/react';
 import {
@@ -6,23 +6,66 @@ import {
   createOutline,
   maleFemaleOutline,
 } from 'ionicons/icons';
-import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
 
 import { useAuthContext } from '../../shared/context/AuthContext';
 import { PROFILE_TAGS } from '../../shared/data/profile-tags';
+import { useGetPosts } from '../../shared/hooks/post/useGetPosts';
 import { useFollowUnfollow } from '../../shared/hooks/users/useFollowUnfollow';
+import { PostCardI, UserItemI } from '../../shared/types';
 import { getFormattedDate } from '../../shared/utils';
+import { usePostStore } from '../../store/usePostStore';
 import { useUserStore } from '../../store/useUserStore';
 import Button from '../ui/button/Button';
+import UsersList from '../users/UsersList';
 import './ProfileCard.css';
 import ProfileCardSkeleton from './ProfileCardSkeleton';
+import ProfilePosts from './ProfilePosts';
+import ProfilePostsSkeleton from './ProfilePostsSkeleton';
 
 const ProfileCard: React.FC<{ userId: string }> = ({ userId }) => {
+  const [activeDetails, setActiveDetails] = useState<
+    'followers' | 'followings' | 'posts'
+  >('posts');
   const { authUser } = useAuthContext();
+  const { getPosts } = useGetPosts();
+  const { id } = useParams<{ id: string }>();
+
   // get info for all users
+  const users = useUserStore((store) => store.users);
+
   const userInfo = useUserStore((store) =>
     store.users.find((user) => user._id === userId)
   );
+
+  // If there is no ID in the URL, use authUser._id (own profile)
+  const currentProfileId = id || authUser?._id;
+
+  // Check if this is a custom profile
+  const isAuthProfile = currentProfileId === authUser?._id;
+
+  const userFollowers =
+    userInfo?.followers
+      ?.map((f) => users.find((user) => user._id === f))
+      ?.filter((user): user is UserItemI => !!user) || []; // filtered if undefined
+
+  const userFollowings =
+    userInfo?.followings
+      ?.map((f) => users.find((user) => user._id === f))
+      ?.filter((user): user is UserItemI => !!user) || []; // filtered if undefined
+  const authUserFollowings =
+    authUser?.followings
+      ?.map((f) => users.find((user) => user._id === f))
+      ?.filter((user): user is UserItemI => !!user) || []; // filtered if undefined
+
+  const allPosts = usePostStore((store) => store.posts);
+  const isPostLoading = usePostStore((store) => store.isLoading);
+
+  const userPosts =
+    userInfo?.posts
+      ?.map((post) => allPosts.find((p) => p._id === post))
+      .filter((post): post is PostCardI => !!post) || []; // filtered if undefined
+
   const {
     handleFollowUnfollow,
     isFollowing,
@@ -46,14 +89,6 @@ const ProfileCard: React.FC<{ userId: string }> = ({ userId }) => {
     return <ProfileCardSkeleton />;
   }
 
-  if (!userInfo) {
-    // Handle the case where userInfo is still undefined even after loading
-    return <div>Error loading user information.</div>;
-  }
-
-  const { _id, fullName, username, bio, gender, birthday, createdAt, posts } =
-    userInfo;
-
   const getProfileTagData = (label: string) => {
     switch (label) {
       case 'followings':
@@ -61,94 +96,121 @@ const ProfileCard: React.FC<{ userId: string }> = ({ userId }) => {
           ? authUser?.followings?.length
           : followingsCount;
       case 'followers':
-        return followersCount;
+        return userInfo?.followers?.length || followersCount;
       case 'posts':
-        return posts?.length || 0;
+        return userInfo?.posts?.length || 0;
       default:
         return 0;
     }
   };
 
+  const followings = isAuthProfile ? authUserFollowings : userFollowings;
+
+  useEffect(() => {
+    if (!allPosts.length) {
+      getPosts();
+    }
+  }, [allPosts]);
+
   return (
     <IonContent>
-      <div className='container'>
-        <div className='flex-end'>
-          {authUser?._id === userId ? (
-            <Button variant='outline' rounded label='Edit Profile' />
-          ) : (
-            <Button
-              variant='secondary'
-              rounded
-              label={isFollowing ? 'Unfollow' : 'Follow'}
-              onClick={handleFollowUnfollow}
-              disabled={isFollowingLoading}
-            />
-          )}
-        </div>
-        <div className='mt-container'>
-          <div className='flex-col'>
-            <p className='text-bold'>{fullName}</p>
-            <p className='text-xs'>@{username}</p>
-          </div>
-          <div className='mt-1-5'>
-            <p>{bio || 'Biography is currently missing'}</p>
-            <div className='flex-wrap-gap'>
-              <div className='flex-row-center'>
-                <IonIcon
-                  icon={maleFemaleOutline}
-                  slot='start'
-                  className='profile-icon'
+      {userInfo && (
+        <>
+          {' '}
+          <div className='container'>
+            <div className='flex-end'>
+              {authUser?._id === userId ? (
+                <Button variant='outline' rounded label='Edit Profile' />
+              ) : (
+                <Button
+                  variant='secondary'
+                  rounded
+                  label={isFollowing ? 'Unfollow' : 'Follow'}
+                  onClick={handleFollowUnfollow}
+                  disabled={isFollowingLoading}
                 />
-                <p className='text-xs-md'>{gender}</p>
-              </div>
-              {birthday && (
-                <div className='flex-row-center'>
-                  <IonIcon
-                    icon={calendarClearOutline}
-                    slot='start'
-                    className='profile-icon'
-                  />
-                  <p className='text-xs-md'>{birthday}</p>
-                </div>
               )}
-              <div className='flex-row-center'>
-                <IonIcon
-                  icon={createOutline}
-                  slot='start'
-                  className='profile-icon'
-                />
-                <p className='text-xs-md'>
-                  Joined {getFormattedDate(createdAt)}
-                </p>
+            </div>
+            <div className='mt-container'>
+              <div className='flex-col'>
+                <p className='text-bold'>{userInfo.fullName}</p>
+                <p className='text-xs'>@{userInfo.username}</p>
               </div>
+              <div className='mt-1-5'>
+                <p>{userInfo.bio || 'Biography is currently missing'}</p>
+                <div className='flex-wrap-gap'>
+                  <div className='flex-row-center'>
+                    <IonIcon
+                      icon={maleFemaleOutline}
+                      slot='start'
+                      className='profile-icon'
+                    />
+                    <p className='text-xs-md'>{userInfo.gender}</p>
+                  </div>
+                  {userInfo.birthday && (
+                    <div className='flex-row-center'>
+                      <IonIcon
+                        icon={calendarClearOutline}
+                        slot='start'
+                        className='profile-icon'
+                      />
+                      <p className='text-xs-md'>{userInfo.birthday}</p>
+                    </div>
+                  )}
+                  <div className='flex-row-center'>
+                    <IonIcon
+                      icon={createOutline}
+                      slot='start'
+                      className='profile-icon'
+                    />
+                    <p className='text-xs-md'>
+                      Joined {getFormattedDate(userInfo.createdAt)}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <IonList className='flex-row-gap list-tags flex-row-center'>
+                {PROFILE_TAGS.map(({ id, label }) => (
+                  <IonItem
+                    key={id}
+                    lines='none'
+                    className='ion-no-padding tag'
+                    color='ion-color-intro-violet'
+                    onClick={() => setActiveDetails(label)}
+                  >
+                    <IonLabel className='stat-module'>
+                      <span className='mr-x stat-number'>
+                        {getProfileTagData(label)}
+                      </span>
+                      {label === 'posts' && getProfileTagData(label) === 1
+                        ? 'Post'
+                        : label.charAt(0).toUpperCase() + label.slice(1)}
+                    </IonLabel>
+                  </IonItem>
+                ))}
+              </IonList>
             </div>
           </div>
-          <IonList className='flex-row-gap list-tags'>
-            {PROFILE_TAGS.map(({ id, label }) => (
-              <IonItem
-                key={id}
-                lines='none'
-                className='ion-no-padding tag'
-                color='ion-color-intro-violet'
-              >
-                <Link
-                  to={`/Profile/${label}/${_id}`}
-                  className='link flex-row-center'
-                >
-                  <IonLabel className='stat-module'>
-                    <span className='mr-x stat-number'>
-                      {getProfileTagData(label)}
-                    </span>
-                    {label === 'posts' && getProfileTagData(label) === 1
-                      ? 'Post'
-                      : label.charAt(0).toUpperCase() + label.slice(1)}
-                  </IonLabel>
-                </Link>
-              </IonItem>
-            ))}
-          </IonList>
-        </div>
-      </div>
+          {activeDetails === 'followers' && (
+            <UsersList users={userFollowers} variant={'followers'} />
+          )}
+          {activeDetails === 'followings' && (
+            <UsersList
+              users={followings}
+              variant={'followings'}
+              isAuthProfile={isAuthProfile}
+            />
+          )}
+          {activeDetails === 'posts' && (
+            <>
+              {!isPostLoading && allPosts.length && (
+                <ProfilePosts posts={userPosts} />
+              )}
+              {isPostLoading && !allPosts.length && <ProfilePostsSkeleton />}
+            </>
+          )}
+        </>
+      )}
     </IonContent>
   );
 };
